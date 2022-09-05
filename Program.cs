ac1e2ac31f8859817827f6b2d155149344b4ff4d
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-09-06 03:53:47 +0600

    Second task solved

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace NinethSeminar
             // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
             // N = 5 -> "5, 4, 3, 2, 1"
             // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-
+            /*
             Console.WriteLine("Задача 64");
             int n = InputNumbers("\nВведите n ");
             int count = 2;
@@ -20,6 +20,40 @@ namespace NinethSeminar
             if (count > n) return;
             PrintNumber(n, count + 1);
             Console.Write(count + ", ");
+            }
+
+            int InputNumbers(string input) {
+            Console.Write(input);
+            int output = Convert.ToInt32(Console.ReadLine());
+            return output;
+            } */
+
+
+
+            // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+            // M = 1; N = 15 -> 120
+            // M = 4; N = 8. -> 30
+
+            Console.WriteLine("Задача 66");
+            int m = InputNumbers("\nВведите m ");
+            int n = InputNumbers("Введите n ");
+            int temp = m;
+
+            if (m > n) {
+            m = n; 
+            n = temp;
+            }
+
+            PrintSumm(m, n, temp=0);
+
+            void PrintSumm(int m, int n, int summ){
+            summ = summ + n;
+            if (n <= m){
+                Console.Write($"Сумма элементов= {summ} ");
+                return;
+            }
+            
+            PrintSumm(m, n - 1, summ);
             }
 
             int InputNumbers(string input) {
