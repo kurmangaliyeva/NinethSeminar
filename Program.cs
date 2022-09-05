062c8b9a5835bcce7832c2cb257adb19d69d6597
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-09-06 03:50:47 +0600

    First task solved

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NinethSeminar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+            // N = 5 -> "5, 4, 3, 2, 1"
+            // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+            Console.WriteLine("Задача 64");
+            int n = InputNumbers("\nВведите n ");
+            int count = 2;
+            PrintNumber(n, count);
+            Console.Write(1);
+
+            void PrintNumber(int n, int count){
+            if (count > n) return;
+            PrintNumber(n, count + 1);
+            Console.Write(count + ", ");
+            }
+
+            int InputNumbers(string input) {
+            Console.Write(input);
+            int output = Convert.ToInt32(Console.ReadLine());
+            return output;
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
